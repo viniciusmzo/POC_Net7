6dd45ea82ba84c7f6e4b6bdd9bb679c7f5d6597a
ViniciusMZO <vinicius_mzo@hotmail.com>
2023-08-03 13:55:18 -0300

    Adding new properties and methods

--- Poc_net7/Entities/DevEvent.cs
+++ Poc_net7/Entities/DevEvent.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public List<DevEventSpeaker> Speakers { get; set; }
@@ -17,7 +18,15 @@
 
         public void Update(string title, string description, DateTime startDate, DateTime endDate) 
         {
-            
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public void Delete() 
+        {
+            IsDeleted = true;
         }
     }
 }
