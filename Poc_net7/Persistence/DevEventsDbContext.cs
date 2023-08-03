bc382022a2ffb8d4b0f6a86ccca83751b66b243d
ViniciusMZO <vinicius_mzo@hotmail.com>
2023-08-03 14:04:10 -0300

    Adding in-memory DbContext

---
+++ Poc_net7/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Poc_net7.Entities;
+
+namespace Poc_net7.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
