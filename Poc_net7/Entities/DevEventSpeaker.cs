3e3f8eb2d541e8b581ffc0f6184499d9dd9e0484
ViniciusMZO <vinicius_mzo@hotmail.com>
2023-08-03 13:59:10 -0300

    Adding new properties to the class DevEventSpeaker

--- Poc_net7/Entities/DevEventSpeaker.cs
+++ Poc_net7/Entities/DevEventSpeaker.cs
@@ -2,5 +2,10 @@
 {
     public class DevEventSpeaker
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TalkTile { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedInProfile { get; set; }
     }
 }
